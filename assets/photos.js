f0cadc5ee09048c5c22b7e0da9bb299c585e0477
nietotzr <nietotzr@gmail.com>
2026-09-26 20:22:58 +0200

    añadir receta huevos rellenos mejillones

--- assets/photos.js
+++ assets/photos.js
@@ -1,5 +1,6 @@
 (function(){
 window.RECIPE_IMAGES={
+ 'huevos-rellenos-mejillones':'assets/img/huevos-rellenos-mejillones.jpeg',
  'arroz-seco-calamar-pulpitos-gambas':'assets/img/arroz-seco-calamar-pulpitos-gambas.webp',
  'solomillo-wellington-ternera':'assets/img/solomillo-wellington-ternera.webp',
  'macarrones-cherrys-calabacin-albahaca-crema-huevo':'assets/img/macarrones-cherrys-calabacin-albahaca-crema-huevo.webp',
